f2cb4d42ee0f9227cdc2093a84993061ad22cfa9
Nury <nurydavletov@gmail.com>
2023-05-10 11:03:51 +0500

    fixed migration

--- database/migrations/2023_05_10_060218_add_column_scan_time_to_table_files.php
+++ database/migrations/2023_05_10_060218_add_column_scan_time_to_table_files.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('files', function (Blueprint $table) {
-            $table->id();
-            $table->string('path')->nullable();
-            // $table->string('scanTime')->nullable();
-            $table->string('local')->nullable();
+        Schema::table('files', function (Blueprint $table) {
+            $table->addColumn('string', 'scanTime')->nullable();
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('files');
+        Schema::table('files', function (Blueprint $table) {
+            $table->dropColumn('scanTime');
+        });
     }
 };
