d4b6a6ecbbd7edd883311fe9ebd4604e1713d033
Nury <nurydavletov@gmail.com>
2023-05-11 16:18:17 +0500

    fixed 7

--- app/Services/Admin/HelperService.php
+++ app/Services/Admin/HelperService.php
@@ -4,9 +4,9 @@ namespace App\Services\Admin;
 
 class HelperService
 {
-    public $pathImageForServer = "/home/nury/nfs/storage2/images/";
-    public $pathTrackForServer = "/home/nury/nfs/storage2/";
+    // public $pathImageForServer = "/home/nury/nfs/storage2/images/";
+    // public $pathTrackForServer = "/home/nury/nfs/storage2/";
     public $pathImageForDb = "https://storage2.ma.st.com.tm/images/";
-    // public $pathImageForServer = "/nfs/storage2/images/";
-    // public $pathTrackForServer = "/nfs/storage2/";
+    public $pathImageForServer = "/nfs/storage2/images/";
+    public $pathTrackForServer = "/nfs/storage2/";
 }
