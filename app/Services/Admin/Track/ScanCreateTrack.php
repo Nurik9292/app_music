968209bd54c860eadcf365b023b9ba902ede9a6c
Nury <nurydavletov@gmail.com>
2023-05-11 14:57:01 +0500

    encode fixed 1

--- app/Services/Admin/Track/ScanCreateTrack.php
+++ app/Services/Admin/Track/ScanCreateTrack.php
@@ -125,6 +125,7 @@ class ScanCreateTrack
 
     private function createAlbum($albums, $isNational)
     {
+        if (!$this->is_utf8($albums)) $albums = 'album';
 
         Log::debug($albums);
         if (mb_detect_encoding($albums) == 'ASCII-8')
@@ -172,5 +173,21 @@ class ScanCreateTrack
         }
 
         return [$path_second_artwork, $path_second_thumb];
+    }
+
+
+
+    function is_utf8($string)
+    {
+        return preg_match('%^(?:
+        [\x09\x0A\x0D\x20-\x7E]
+        | [\xC2-\xDF][\x80-\xBF]
+        | \xE0[\xA0-\xBF][\x80-\xBF]
+        | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}
+        | \xED[\x80-\x9F][\x80-\xBF]
+        | \xF0[\x90-\xBF][\x80-\xBF]{2}
+        | [\xF1-\xF3][\x80-\xBF]{3}
+        | \xF4[\x80-\x8F][\x80-\xBF]{2}
+        )*$%xs', $string);
     }
 }
