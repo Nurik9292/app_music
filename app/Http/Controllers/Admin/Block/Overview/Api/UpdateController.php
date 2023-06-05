a0f3620db4c2e72fb1081551d12e44b05069c684
Nury <nurydavletov@gmail.com>
2023-06-05 17:09:26 +0500

    gooooooooooooood

--- app/Http/Controllers/Admin/Block/Overview/Api/UpdateController.php
+++ app/Http/Controllers/Admin/Block/Overview/Api/UpdateController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin\Block\Overview\Api;
 use App\Http\Controllers\Controller;
 use App\Models\BlockShema;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use OwenIt\Auditing\Models\Audit;
 
 class UpdateController extends Controller
@@ -22,15 +21,17 @@ class UpdateController extends Controller
             'albums' => $data['albums'] ?? '',
             'playlists' => $data['playlists'] ?? '',
             'tracks' => $data['tracks'] ?? '',
-            'genres' => $data['genres'] ?? '',
+            'artists' => $data['artists'] ?? '',
         ]);
 
-        unset($data['playlists'], $data['albums'], $data['tracks'], $data['genres'], $data['name_status']);
+        unset($data['playlists'], $data['albums'], $data['tracks'], $data['artists'], $data['name_status']);
 
-        if (count(BlockShema::where('order_number', $data['order_number'])->get()) > 0)
+        if (count(BlockShema::where('order_number', $data['order_number'])->get()) > 0) {
             $blocks = BlockShema::where('order_number', '>=', $data['order_number'])->get();
+            foreach ($blocks as $block)
+                $block->increment('order_number');
+        }
 
-        foreach ($blocks as $block) $block->increment('oreder_number');
 
         $block->update($data);
 
